83363a22efca0d8819bf4e802d70003f137f54e6
Art <artelo.studio@gmail.com>
2019-07-03 20:24:39 +0000

    solved

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import './App.css';
-import Nav from './components/Nav';
-import List from './components/List';
-import Note from './components/NoteCard';
-import axios from 'axios';
-import urlFor from './helpers/urlFor';
+import React, { Component } from "react";
+import "./App.css";
+import Nav from "./components/Nav";
+import List from "./components/List";
+import Note from "./components/NoteCard";
+import axios from "axios";
+import urlFor from "./helpers/urlFor";
 
 class App extends Component {
   constructor() {
@@ -15,29 +15,29 @@ class App extends Component {
     };
   }
 
-toggleNote = () => {
-  this.setState({
-    showNote: !this.state.showNote
-  });
-}
+  toggleNote = () => {
+    this.setState({
+      showNote: !this.state.showNote
+    });
+  };
 
-getNotes = () => {
-  axios.get(urlFor('notes'))
-  .then((res) => this.setState({ notes: res.data }) )
-  .catch((err) => console.log(err.response.data) );
-  }
+  getNotes = () => {
+    axios
+      .get(urlFor("notes"))
+      .then(res => this.setState({ notes: res.data }))
+      .catch(err => console.log(err.response.data));
+  };
 
   render() {
     const { showNote, notes } = this.state;
 
     return (
-    <div className="App">
-    <Nav toggleNote={this.toggleNote} showNote={showNote} />
-      { showNote ? <Note /> : <List getNotes={this.getNotes}/> }
-        // notes={notes}
-    </div>
-  );
-}
+      <div className="App">
+        <Nav toggleNote={this.toggleNote} showNote={showNote} />
+        {showNote ? <Note /> : <List getNotes={this.getNotes} notes={notes} />}
+      </div>
+    );
+  }
 }
 
 export default App;
