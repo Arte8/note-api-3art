433f26e088ee77e0e7e469944fd91b334f7795e7
Art <artelo.studio@gmail.com>
2019-07-03 01:25:40 +0000

    fix try urlFor

--- src/helpers/urlFor.js
+++ src/helpers/urlFor.js
@@ -1,6 +1,6 @@
 const urlFor = (endpoint) => {
-  return 'https://note-app-3art.herokuapp.com/' + endpoint;
-  // return 'http://localhost:3000/' + endpoint;
+  // return 'https://note-app-3art.herokuapp.com/' + endpoint;
+  return 'http://localhost:3000/' + endpoint;
 };
 
 export default urlFor;
